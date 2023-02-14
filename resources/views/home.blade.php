1fc3a17e3f29ee721c1c75be8c5ccf87cc787faf
Mampuya wa Lungila <mampuyawalungila@gmail.com>
2023-02-14 18:17:23 +0100

    creation du modele MVC sur mon premier projet laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html>
+    <title>
+        Mon premier projet en Laravel
+    </title>
+    <body>
+        <table>
+    <thead>
+        <tr>
+            <th colspan="2">Tableau des posts</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+        </tr>
+    </tbody>
+</table>
+
+    </body>
+</html>
