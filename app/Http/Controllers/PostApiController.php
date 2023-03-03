2cfa7744214d91bf727b9430067396a5254c38b0
Mampuya wa Lungila <mampuyawalungila@gmail.com>
2023-03-03 11:38:17 +0100

    commit creation d'un article

--- app/Http/Controllers/PostApiController.php
+++ app/Http/Controllers/PostApiController.php
@@ -3,25 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\PostApis;
 use App\Models\Post;
 
 class PostApiController extends Controller
 {
-    public function index(){
-        $post= PostApis::all();
 
-        return response($post, 200);
-    }
-
-    public function test()
+    public function index()
     {
         $post= Post::all();
 
         return response($post, 200);
 
     }
-
 
 
     public function insert (Request $request)
@@ -31,15 +24,13 @@ class PostApiController extends Controller
             'contenu' => 'required|min:5'
         ]);
 
-        $post= new PostApis;
+        $post= new Post;
 
         $post->titre = $postData['titre'];
         $post->contenu = $postData['contenu'];
         $post->save();
 
         return response(['message' => 'Un post est ajouté avec succès']);
-
-
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/store/index', [PostApiController::class, 'index']);
 
-Route::get('/store/test', [PostApiController::class, 'test']);
-
 Route::post('/store/insert', [PostApiController::class, 'insert']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
